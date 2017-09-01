e921cd3dfe41c2f35b2c366143308d2757db3902
Simon <thatguywiththatname@users.noreply.github.com>
2017-09-01 18:30:57 +0100

    Update ink.py

--- Ink/ink.py
+++ Ink/ink.py
@@ -54,7 +54,7 @@ ROWS    = 40
 
 RESET_TO_TOP_LEFT = "\x1b[0;0H"
 
-if platform == "linux":
+if platform == "linux" or platform == "darwin":
     CLR = "clear"
 elif platform == "win32":
     CLR = "cls"
