438f4d911526137e28d8f9f46fcf2ed64aa6b6c8
Simon <simjenner3@gmail.com>
2017-10-07 23:06:51 +0100

    Optimised

--- Colonies/p5.js/sketch.js
+++ Colonies/p5.js/sketch.js
@@ -1,23 +1,20 @@
-// In this script, 0 = male, 1 = female
-var MALE = 0;
-var FEMALE = 1;
-var BREEDING_DELAY = 1;  // waits this amount of gens before breeding again
-var MUTATION_CHANCE = 1;  // 1/MUTATION_CHANCE+1 chance of new cells mutating
-var MUT_RANGE = 5; // cells health & strength can change from -MUT_RANGE to MUT_RANGE
-
-var cells = [];
-var GEN = 0;
-var CA = 60 // CA grid is CAxCA then upscaled to VIS (so CA*CA amount of cells)
-var VIS = 800; // Visual grid is VISxVIS (VIS*VIS amount of pixels)
-
 function setup() {
-  // Variables that need to be re-initialized when setup() is called
-  // CA_ROWS and CA_COLUMNS could be one variable but are named for clarity
+  // In this script, 0 = male, 1 = female
+  MALE = 0;
+  FEMALE = 1;
+  MUTATION_CHANCE = 1;  // 1/MUTATION_CHANCE+1 chance of new cells mutating
+
+  cells = [];
+  GEN = 0;
+  CA = 60 // CA grid is CAxCA then upscaled to VIS (so CA*CA amount of cells)
+  VIS = 800; // Visual grid is VISxVIS (VIS*VIS amount of pixels)
+
+  // CA_ROWS and CA_COLUMNS could be one variable but are different for clarity
   CA_ROWS = CA;
   CA_COLUMNS = CA;
   CELL_SIZE = VIS/CA;
 
-  // 2 colours per colony, for male & female. Female = lighter
+  // HSB colors for each colony (orange, green, blue, pink)
   COLONIES = [
     [40, 78, 10],
     [93, 78, 10],
@@ -35,8 +32,8 @@ function setup() {
 
 function draw() {
   document.getElementById("genh1").innerHTML = "Generation: "+GEN;
-  for (var r = 0; r < CA_ROWS; r++) {
-    for (var c = 0; c < CA_COLUMNS; c++) {
+  for (let r = 0; r < CA_ROWS; r++) {
+    for (let c = 0; c < CA_COLUMNS; c++) {
       if (cells[r][c] != 0) {
         fill(cells[r][c].colour);
       } else {
@@ -55,12 +52,11 @@ function cell(colony, strength, health) {
   this.health = health;
   this.age = 0;
   this.sex = rand_int(0, 1);
-  this.gsb = 0;  // gsb = gens since breeding
 
   // Brightness depends on strength
   let base = COLONIES[colony];
-  if ((base[2] + (strength*2)) > 255) {
-    this.colour = color(base[0], base[1], 255);
+  if ((base[2] + (strength*2)) > 100) {
+    this.colour = color(base[0], base[1], 100);
   } else {
     this.colour = color(base[0], base[1], base[2] + (strength*2));
   }
@@ -71,47 +67,45 @@ function rand_int(min, max) {
 }
 
 function init_cells() {
-  for (var r = 0; r < CA_ROWS; r++) {
+  for (let r = 0; r < CA_ROWS; r++) {
     clmns = [];
-    for (var c = 0; c < CA_COLUMNS; c++) {
+    for (let c = 0; c < CA_COLUMNS; c++) {
       clmns.push(0);
     }
     cells.push(clmns);
   }
-  for (var i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     cells[0][i] = new cell(0, 5, 10);
   }
-  for (var i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     cells[CA_ROWS-1][i] = new cell(1, 5, 10);
   }
-  for (var i = 1; i < 11; i++) {
+  for (let i = 1; i < 11; i++) {
     cells[0][CA_COLUMNS-i] = new cell(2, 5, 10);
   }
-  for (var i = 1; i < 11; i++) {
+  for (let i = 1; i < 11; i++) {
     cells[CA_ROWS-1][CA_COLUMNS-i] = new cell(3, 5, 10);
   }
 }
 
-/*
-  NEIGHBOUR FUNCTIONS
-*/
+// NEIGHBOUR FUNCTIONS
 function neighbouring_cells(r, c) {
   // Returns neighbours in random order
-  var rn = [];
-  var n = [[r-1, c-1], [r-1, c], [r-1, c+1],
+  let rn = [];
+  let n = [[r-1, c-1], [r-1, c], [r-1, c+1],
            [r, c-1], [r, c+1],
            [r+1, c-1], [r+1, c], [r+1, c+1]];
-  var nl = n.length;
-  for (var i = 0; i < nl; i++) {
-    var ri = rand_int(0, n.length-1);
+  let nl = n.length;
+  for (let i = 0; i < nl; i++) {
+    let ri = rand_int(0, n.length-1);
     rn.push(n.splice(ri, 1)[0]);
   }
   return rn;
 }
 
 function get_male_neighbour(r, c, colony) {
-  var n = neighbouring_cells(r, c);
-  for (var i = 0; i < n.length; i++) {
+  let n = neighbouring_cells(r, c);
+  for (let i = 0; i < n.length; i++) {
     if (n[i][0] != -1 && n[i][1] != -1 && n[i][0] != CA_ROWS && n[i][1] != CA_COLUMNS && cells[n[i][0]][n[i][1]] != 0) {
       if (cells[n[i][0]][n[i][1]].sex == MALE && cells[n[i][0]][n[i][1]].colony == colony) {
         return n[i];
@@ -122,8 +116,8 @@ function get_male_neighbour(r, c, colony) {
 }
 
 function get_empty_neighbour(r, c) {
-  var n = neighbouring_cells(r, c);
-  for (var i = 0; i < n.length; i++) {
+  let n = neighbouring_cells(r, c);
+  for (let i = 0; i < n.length; i++) {
     if (n[i][0] != -1 && n[i][1] != -1 && n[i][0] != CA_ROWS && n[i][1] != CA_COLUMNS) {
       if (cells[n[i][0]][n[i][1]] == 0) {
         return n[i];
@@ -134,8 +128,8 @@ function get_empty_neighbour(r, c) {
 }
 
 function get_neighbouring_enemy(r, c, colony) {
-  var n = neighbouring_cells(r, c);
-  for (var i = 0; i < n.length; i++) {
+  let n = neighbouring_cells(r, c);
+  for (let i = 0; i < n.length; i++) {
     if (n[i][0] != -1 && n[i][1] != -1 && n[i][0] != CA_ROWS && n[i][1] != CA_COLUMNS && cells[n[i][0]][n[i][1]] != 0) {
       if (cells[n[i][0]][n[i][1]].colony != colony) {
         return n[i];
@@ -144,54 +138,51 @@ function get_neighbouring_enemy(r, c, colony) {
   }
   return false;
 }
-/*
-  END
-*/
+// END
 
 function tick() {
-  // Breeding stage (mutations need fixing)
-  var breeding_change = [];
-  for (var r = 0; r < CA_ROWS; r++) {
-    for (var c = 0; c < CA_COLUMNS; c++) {
-      var cc = cells[r][c];  // cc = current cell
+  // Breeding stage
+  let breeding_change = [];
+  for (let r = 0; r < CA_ROWS; r++) {
+    for (let c = 0; c < CA_COLUMNS; c++) {
+      let cc = cells[r][c];  // cc = current cell
       if (cc != 0) {
         if (cc.sex == FEMALE) {
-          cc.gsb++;
-          var mn = get_male_neighbour(r, c, cc.colony);
-          var en = get_empty_neighbour(r, c);
-          if (mn && en && cc.gsb >= BREEDING_DELAY) {
-            cc.gsb = 0;
-
-            // Mutation
-            var e_strength = cc.strength;
-            if (rand_int(0, MUTATION_CHANCE) == 0) {
-              if (e_strength > 0) {
-                e_strength += [-1, 1][rand_int(0, 1)];
-              } else {
+          let mn = get_male_neighbour(r, c, cc.colony);
+          let en = get_empty_neighbour(r, c);
+          if (mn && en) {
+            // Chlid mutation
+            let e_strength = cc.strength;
+            if (!rand_int(0, MUTATION_CHANCE)) {
+              if (e_strength == 0) {
                 e_strength += 1;
+              } else if (e_strength == 100) {
+                e_strength -= 1;
+              } else {
+                // Otherwise, random mutation
+                e_strength += [-1, 1][rand_int(0, 1)];
               }
             }
-
             breeding_change.push([en[0], en[1], cc.colony, e_strength, cc.health]);
           }
         }
       }
     }
   }
-  //// Create cells after calculations to prevent skew to left
-  for (var i = 0; i < breeding_change.length; i++) {
-    var breeding_arr = breeding_change[i];
+  // // Create cells after calculations to prevent skew to left
+  for (let i = 0; i < breeding_change.length; i++) {
+    let breeding_arr = breeding_change[i];
     cells[breeding_arr[0]][breeding_arr[1]] = new cell(breeding_arr[2], breeding_arr[3], breeding_arr[4]);
   }
   // END
 
   // Fighting stage
-  var cells_to_kill = []
-  for (var r = 0; r < CA_ROWS; r++) {
-    for (var c = 0; c < CA_COLUMNS; c++) {
-      var cc = cells[r][c];
+  let cells_to_kill = []
+  for (let r = 0; r < CA_ROWS; r++) {
+    for (let c = 0; c < CA_COLUMNS; c++) {
+      let cc = cells[r][c];
       if (cc != 0) {
-        var en = get_neighbouring_enemy(r, c, cc.colony);
+        let en = get_neighbouring_enemy(r, c, cc.colony);
         if (en != false) {
           en_c = cells[en[0]][en[1]];  // en_c = enemy neighbour cell
           cc.health -= en_c.strength;
@@ -206,17 +197,17 @@ function tick() {
       }
     }
   }
-  //// Destroy cells after calculations to prevent skew to left
-  for (var i = 0; i < cells_to_kill.length; i++) {
+  // // Destroy cells after calculations to prevent skew to left
+  for (let i = 0; i < cells_to_kill.length; i++) {
     cells[cells_to_kill[i][0]][cells_to_kill[i][1]] = 0;
   }
   // END
 
-  // For each colony, amount of cells, total strength, total health, total age (also age all cells by 1 gen)
-  var colony_totals = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
-  for (var r = 0; r < CA_ROWS; r++) {
-    for (var c = 0; c < CA_COLUMNS; c++) {
-      var cc = cells[r][c];
+  // For each colony, amount of cells, total strength, total age (also age all cells by 1 gen)
+  let colony_totals = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  for (let r = 0; r < CA_ROWS; r++) {
+    for (let c = 0; c < CA_COLUMNS; c++) {
+      let cc = cells[r][c];
       if (cc != 0) {
         cells[r][c].age += 1;
         colony_totals[cc.colony][0] += 1;
